519ac110e9e4e90245cedff9081936a0170292ad
Denis Craig <denismcraig@gmail.com>
2015-10-27 13:47:07 +0000

    Added cache configuration. Updated app test to store some stuff in the session.

--- application/index.php
+++ application/index.php
@@ -2,6 +2,15 @@
 
 require_once('./config.php');
 
+session_start();
+
+if (!isset($_SESSION['message'])) {
+  $_SESSION['message'] = 'test';
+  echo 'Session message set. Refresh page to see.';
+} else {
+  echo $_SESSION['message'];
+}
+
 $link = mysqli_connect(DB_HOSTNAME, 'root', 'B4c0n1234', 'test');
 
 if (!$link) {
